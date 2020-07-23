271664e0a1a8dbd3cdee68a0e3599a78b1135b6a
Ximena Puchaicela <hxime16@gmail.com>
2020-07-23 11:54:27 -0500

    biometrico

--- xodin/settings.py
+++ xodin/settings.py
@@ -13,8 +13,8 @@ from django.urls import reverse_lazy
 import os
 
 # heroku
-# import django_heroku
-# import dj_database_url
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
